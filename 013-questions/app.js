6798d987237e74aacf012d260219e16cb194ce4a
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2022-05-17 15:58:02 +0100

    Atualização dos exercícios
    Projeto 13 - Questions
    Projeto 14 - Menu

--- 013-questions/app.js
+++ 013-questions/app.js
@@ -1,10 +1,43 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll('.question-btn')
+// const btns = document.querySelectorAll('.question-btn')
 
-btns.forEach(function(btn){
-  btn.addEventListener('click', function(e) {
-    console.log(e.currentTarget)
-  })
-})
+// btns.forEach(function(btn){
+//   btn.addEventListener('click', function(e) {
+//     const question = e.currentTarget.parentElement.parentElement
+//     question.classList.toggle('show-text')
+//   })
+// })
+
+// const questions = document.querySelectorAll('.question')
+
+// questions.forEach(function(quest) {
+//   const btn = quest.querySelector('.question-btn')
+//   btn.addEventListener('click', function() {
+//     quest.classList.toggle('show-text')
+//   })
+// })
+
+// const questions = document.querySelectorAll('.question')
+
+// questions.forEach(function(quest) {
+//   const btn = quest.querySelector('.question-btn')
+//   btn.addEventListener('click', function() {
+//     quest.classList.toggle('show-text')
+//   })
+// })
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (quest) {
+  const btn = quest.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function(item) {
+      if(quest !== item) {
+        item.classList.remove('show-text')
+      }
+    })
+    quest.classList.toggle('show-text')
+  });
+});
