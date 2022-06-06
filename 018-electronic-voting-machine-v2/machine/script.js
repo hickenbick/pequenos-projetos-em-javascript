b0b8b44476a203d2a33d97e45df9095fc1a8d2da
Rodrigo Hickenbick <rodrigo.hickenbick@gmail.com>
2022-06-06 19:24:13 +0100

    Conclusão da urna eletrônica

--- 018-electronic-voting-machine-v2/machine/script.js
+++ 018-electronic-voting-machine-v2/machine/script.js
@@ -3,15 +3,18 @@ let numeros = document.querySelector(".d-1-3");
 let cargo = document.querySelector(".d-1-2");
 let descricao = document.querySelector(".d-1-4");
 let aviso = document.querySelector(".d-2");
-let lateral = document.querySelectorAll(".d-1-right");
+let lateral = document.querySelector(".d-1-right");
 
 let etapaAtual = 0;
 let numero = "";
+let votoBranco = false;
 
 function comecarEtapa() {
   let etapa = etapas[etapaAtual];
 
   let numeroHtml = "";
+  numero = "";
+  votoBranco = false;
 
   for (let i = 0; i < etapa.numeros; i++) {
     if (i === 0) {
@@ -20,11 +23,12 @@ function comecarEtapa() {
       numeroHtml += '<div class="n"></div>';
     }
   }
-  seuVotoPara.innerHTML = "";
+  seuVotoPara.innerHTML = "Digite no teclado seu voto para:";
   descricao.innerHTML = "";
-  aviso.innerHTML = "";
+  aviso.style.display = "none";
   lateral.innerHTML = "";
   numeros.innerHTML = numeroHtml;
+  lateral.innerHTML = "";
 }
 
 function atualizaInterface() {
@@ -36,7 +40,23 @@ function atualizaInterface() {
       return false;
     }
   });
-  console.log(candidato)
+  if (candidato.length > 0) {
+    candidato = candidato[0];
+    seuVotoPara.innerHTML = "SEU VOTO PARA:";
+    aviso.style.display = "block";
+    descricao.innerHTML = `Nome: ${candidato.nome} <br> Partido: ${candidato.partido}`;
+
+    let fotosHTML = "";
+    for (let i in candidato.fotos) {
+      fotosHTML += `<div class="d-1-img"><img src="${candidato.fotos[i].url}" alt="...">${candidato.fotos[i].legenda}</div>`;
+    }
+    lateral.innerHTML = fotosHTML;
+  } else {
+    candidato = null;
+    seuVotoPara.innerHTML = "SEU VOTO PARA:";
+    aviso.style.display = "block";
+    descricao.innerHTML = `Seu voto será <strong>ANULADO</strong>.`;
+  }
 }
 
 function clicou(n) {
@@ -49,6 +69,40 @@ function clicou(n) {
       elNumero.nextElementSibling.classList.add("pisca");
     } else {
       atualizaInterface();
+    }
+  }
+}
+
+function branco() {
+  if (numero === "") {
+    votoBranco = true;
+    seuVotoPara.innerHTML = "SEU VOTO PARA:";
+    aviso.style.display = "block";
+    numeros.innerHTML = "";
+    descricao.innerHTML = `Voto em <strong>BRANCO</strong>.`;
+  } else {
+    alert("Antes de votam em branco pressione o botão CORRIGE.");
+  }
+}
+function corrige() {
+  comecarEtapa();
+}
+function confirma() {
+  let etapa = etapas[etapaAtual];
+  let votoConfirmado = false;
+  if (votoBranco === true) {
+    votoConfirmado = true;
+    console.log("confirmando como BRANCO...");
+  } else if (numero.length === etapa.numeros) {
+    votoConfirmado = true;
+    console.log("Confirmando como: " + numero);
+  }
+  if(votoConfirmado) {
+    etapaAtual++
+    if(etapas[etapaAtual] !== undefined) {
+      comecarEtapa();
+    } else {
+      document.querySelector('.tela').innerHTML = '<div class="aviso--grande">FIM!</div>';
     }
   }
 }
